c8edddca1e8b06c2752125cbf17117a14298e2e5
Malphegal <nateag_96@hotmail.fr>
2020-02-17 10:22:54 +0100

    Correction de typo.

--- 9.php
+++ 9.php
@@ -6,9 +6,9 @@
     echo "Sexe : $sexe<br/>";
 
     if ($sexe == 'M')
-        echo "La personnes " . ($age > 20 ? "est imposable." : "n'est pas imposable.");
+        echo "La personne " . ($age > 20 ? "est imposable." : "n'est pas imposable.");
     else if ($sexe == 'F')
-        echo "La personnes " . ($age >= 18 && $age <= 35 ? "est imposable." : "n'est pas imposable.");
+        echo "La personne " . ($age >= 18 && $age <= 35 ? "est imposable." : "n'est pas imposable.");
     else
         echo "Il y a une erreur dans le sexe de la personne ! (valeur : '$sexe')";
 ?>
